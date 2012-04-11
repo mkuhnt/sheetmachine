3decdedf9c1d8dc19c077d61d079506ef120c467
Markus Kuhnt <markus.kuhnt@tridoco.com>
2012-04-11 09:17:50 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sheetmachine::Application.config.secret_token = 'fc114e85ae2c0ca8fe85469449d8f0eae2645b80b761940e78c66119bbe87eee8a0cc87184458cba01eec62446747f1156029c680fa6ff03bef58a43403839e3'
