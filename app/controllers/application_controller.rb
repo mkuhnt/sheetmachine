c4681f43ebce73b30ffe690b683ecdcb5183e165
Markus Kuhnt <markus.kuhnt@tridoco.com>
2012-04-14 10:23:17 +0200

    Added pjax and implemented first navigation cases

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :set_layout
+
+private
+  def set_layout
+    if request.headers['X-PJAX']
+      false
+    else
+      "application"
+    end
+  end
 end
